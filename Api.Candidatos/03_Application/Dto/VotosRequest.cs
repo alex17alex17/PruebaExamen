2a70cfcafc4a73f60709679e127eb051a36dcf70
alexsq17 <alexsq17@hotmail.com>
2025-05-28 11:36:27 +0200

    commit 2

--- Api.Candidatos/03_Application/Dto/VotosRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Api.Candidatos._03_Application.Dto;
-
-public class VotosRequest
-{
-    public required int Votes { get; set; }
-    public required int Id { get; set; }
-}
--- Api.Candidatos/03_Application/Services/IDatabaseScriptService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Api.Candidatos._03_Application.Services
-{
-    public interface IDatabaseScriptService
-    {
-        public Task EjecutarScriptAsync(string scriptFileName);
-    }
-}
-
